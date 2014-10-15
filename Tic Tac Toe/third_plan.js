e8ee35168d667078f2c198d43d106d83cb1d65d0
MavisZeng <mayzeng914@hotmail.com>
2014-10-14 20:10:50 -0700

    firebase works on players' name & turns

--- Tic Tac Toe/third_plan.js
+++ Tic Tac Toe/third_plan.js
@@ -4,8 +4,21 @@ tttapp.controller('tttController', function($scope, $timeout, $firebase) {
 
 var firebaseRef = new Firebase("https://shining-fire-3332.firebaseio.com/");
 
-$scope.remoteBoxesContainer = 
-    $firebase(new Firebase("https://shining-fire-3332.firebaseio.com" + '/remoteBoxesContainer')) ;
+ $scope.remoteBoxesContainer = 
+     $firebase(new Firebase("https://shining-fire-3332.firebaseio.com" + '/remoteBoxesContainer')) ;
+
+//game starts
+var howManyClicks = 0;
+
+//define turns
+var turnStatus = '';
+
+//initial status
+$scope.player1Status = "player 1";
+$scope.player2Status = "player 2";
+var player_name_1 = '';
+var player_name_2 = '';
+$scope.tie_count = "Tie";
 
 //created the board
 $scope.boxes = [{
@@ -28,42 +41,41 @@ $scope.boxes = [{
         clicked: false
     }];
 
-$scope.boxesContainer = {
-    boxList: $scope.boxes
-};
-
-$scope.remoteBoxesContainer.$bind($scope, "boxesContainer");
-$scope.$watch('boxesContainer', function() {
-    console.log('things changed!');
-});
-
-//define turns
-var turnStatus = '';
+// //define turns
+// var turnStatus = '';
 
 //define player 1's turn
 function turning1(){
-    turnStatus = player_name_1 + "'s turn";
-    $scope.turn_status = turnStatus;
+    turnStatus = $scope.boxesContainer.player1 + "'s turn";
+    $scope.boxesContainer.turning = turnStatus;
     turncolor.colorpink = true;
     turncolor.colorblue = false;
-    console.log("pink " + turncolor.colorpink + " and blue " + turncolor.colorblue);
+    console.log($scope.boxesContainer.player1 + "'s turn"); 
 };
 
 //define player 2's turn
 function turning2(){
-    turnStatus = player_name_2 + "'s turn";
-    $scope.turn_status = turnStatus;
+    turnStatus = $scope.boxesContainer.player2 + "'s turn";
+    $scope.boxesContainer.turning = turnStatus;
     turncolor.colorpink = false;
     turncolor.colorblue = true;
-    console.log("pink " + turncolor.colorpink + " and blue " + turncolor.colorblue);
+    console.log($scope.boxesContainer.player2 + "'s turn");
 };
 
 var turncolor = [];
 
-//initial status
-var player_name_1 = "player 1";
-var player_name_2 = "player 2";
-$scope.tie_count = "Tie";
+$scope.boxesContainer = {
+     boxList: $scope.boxes,
+     clickCounter: howManyClicks,
+     player1: player_name_1,
+     player2: player_name_2,
+     turning: turnStatus  
+ };
+
+$scope.remoteBoxesContainer.$bind($scope, "boxesContainer");
+// $scope.$watch('boxesContainer', function() {
+//     console.log('things changed!');
+// });
    
 //Beginning prompts
 window.onload = function() {
@@ -72,44 +84,37 @@ window.onload = function() {
 
 //player 1 check in
 function promptPlayer1() { 
-        player_name_1 = prompt("Player 1, enter your name below\n(no more than 10 characters):");
-        if ( (player_name_1 == null) || (player_name_1.length > 10) || (player_name_1.length == 0) ) {
+        $scope.boxesContainer.player1 = prompt("Player 1, enter your name below\n(no more than 10 characters):");
+        if ( ($scope.boxesContainer.player1 == null) || ($scope.boxesContainer.player1.length > 10) || ($scope.boxesContainer.player1.length == 0) ) {
             promptPlayer1();
         }
         else {
-            $scope.player1Status = player_name_1 + ": " + player1Win;
+            $scope.player1Status = $scope.boxesContainer.player1 + ": " + player1Win;
             promptPlayer2();
         };
 };
 
 //player 2 check in
 function promptPlayer2() {
-        player_name_2 = prompt("Player 2, enter your name below\n(no more than 10 characters):");
-        if ( (player_name_2 == null) || (player_name_2.length > 10) || (player_name_2.length == 0) ) {
+        $scope.boxesContainer.player2 = prompt("Player 2, enter your name below\n(no more than 10 characters):");
+        if ( ($scope.boxesContainer.player2 == null) || ($scope.boxesContainer.player2.length > 10) || ($scope.boxesContainer.player2.length == 0) ) {
             promptPlayer2();
         }
         else {
-            $scope.player2Status = player_name_2 + ": " + player2Win;
+            $scope.player2Status = $scope.boxesContainer.player2 + ": " + player2Win;
             $scope.tie_count = "Tie: " + howManyTies;
-            turnStatus = player_name_1 + " goes first!";
-            $scope.turn_status = turnStatus;
+            turnStatus = $scope.boxesContainer.player1 + " goes first!";
+            $scope.boxesContainer.turning = turnStatus;
             turncolor.colorpink = true;
-            console.log("pink " + turncolor.colorpink);
         };
 };
 
 //game is ready
 var player1Win = 0;
-$scope.player1Status = player_name_1; 
 var player2Win = 0;
-$scope.player2Status = player_name_2; 
-
 var howManyTies = 0;
 var player1WinCount = 0;
-var player2WinCount = 0;
-
-//game starts
-var howManyClicks = 0;
+var player2WinCount = 0; 
 
 $scope.clickIt = function(box) {
     // if our box has been clicked if box.clicked
@@ -122,11 +127,11 @@ $scope.clickIt = function(box) {
             // else box.even = true
     if (box.clicked == false) {
         box.clicked = true;
-        howManyClicks++;
+        $scope.boxesContainer.clickCounter++;
         // howManyClicks => 1
 
         // odd check
-        if (howManyClicks % 2 == 1) {
+        if ($scope.boxesContainer.clickCounter % 2 == 1) {
             //player 1
             box.odd = true;
             //check player 1's winning status
@@ -166,16 +171,16 @@ $scope.clickIt = function(box) {
                     player1WinCount++;
             };
             //change to player 2's turn
-            if ( (howManyClicks < 9) && (player1WinCount == 0) ) {
+            if ( ($scope.boxesContainer.clickCounter < 9) && (player1WinCount == 0) ) {
                 turning2(); 
             };
             //game is tied
-            if ( (howManyClicks == 9) && (player1WinCount == 0) ) {
+            if ( ($scope.boxesContainer.clickCounter == 9) && (player1WinCount == 0) ) {
                 howManyTies++;
                 $scope.tie_count = "Tie: " + howManyTies;
                 turnStatus = "It's tied!";
                 nextRound();
-                $scope.turn_status = turnStatus;
+                $scope.boxesCountainer.turning = turnStatus;
                 turncolor.colorpink = false;
                 urncolor.colorblue = false;
                 console.log("pink " + turncolor.colorpink + " and blue " + turncolor.colorblue);
@@ -241,10 +246,10 @@ $scope.clickIt = function(box) {
 //when player 1 wins
 pinkWin = function () {
     $timeout(function() {
-        turnStatus = player_name_1 + " (pink) win!";
-        $scope.turn_status = turnStatus;
+        turnStatus = $scope.boxesContainer.player1 + " (pink) win!";
+        $scope.boxesContainer.turning = turnStatus;
         player1Win++;
-        $scope.player1Status = player_name_1 + ": " + player1Win;
+        $scope.player1Status = $scope.boxesContainer.player1 + ": " + player1Win;
         for ( i = 0; i <= 8; i++ ) {
             if ($scope.boxes[i].clicked == false) {
                 $scope.boxes[i].clicked = true;
@@ -261,10 +266,10 @@ pinkWin = function () {
 //when player 2 wins
 blueWin = function () {
    $timeout(function() {
-        turnStatus = player_name_2 + " (blue) win!";
-        $scope.turn_status = turnStatus;
+        turnStatus = $scope.boxesContainer.player2 + " (blue) win!";
+        $scope.boxesContainer.turning = turnStatus;
         player2Win++;
-        $scope.player2Status = player_name_2 + ": " + player2Win;
+        $scope.player2Status = $scope.boxesContainer.player2 + ": " + player2Win;
         for ( i = 0; i <= 8; i++ ) {
             if ($scope.boxes[i].clicked == false) {
                 $scope.boxes[i].clicked = true;
@@ -296,18 +301,18 @@ function nextRound() {
 
 //only reload the board
 $scope.clickToReloadBoard = function() {
-    if ( (player1WinCount > 0) || (player2WinCount > 0) || (howManyClicks == 9) ) { 
+    if ( (player1WinCount > 0) || (player2WinCount > 0) || ($scope.boxesContainer.clickCounter == 9) ) { 
         for ( i = 0; i < 9; i++ ) {
             $scope.boxes[i].odd = false;
             $scope.boxes[i].even = false;
             $scope.boxes[i].clicked = false;
-            howManyClicks = 0;
+            $scope.boxesContainer.clickCounter = 0;
             player1WinCount = 0;
             player2WinCount = 0;
             turncolor.colorpink = true;
             turncolor.colorblue = false;
-            turnStatus = player_name_1 + " goes first!";
-            $scope.turn_status = turnStatus;
+            turnStatus = $scope.boxesContainer.player1 + " goes first!";
+            $scope.boxesContainer.turning = turnStatus;
             console.log("pink " + turncolor.colorpink);
         };
     }
@@ -318,13 +323,13 @@ $scope.clickToReloadBoard = function() {
 
 //reload the page
 $scope.clickToReloadPage = function() {
-    if ( (player1WinCount > 0) || (player2WinCount > 0) || (howManyClicks == 9) ) {
+    if ( (player1WinCount > 0) || (player2WinCount > 0) || ($scope.boxesContainer.clickCounter == 9) ) {
         var playWithNew = confirm("Are you sure you wanna leave the game right now?\nYou still have " + ( 3 - player1Win - player2Win - howManyTies ) + " round(s) to go!\nClick \"OK\" to continue the game;\nClick \"Cancel\" to start a new game with someone else.");
         if (!playWithNew) {
             window.location.reload();
         };
     }
-    else if ( (player1Win == 0) && (player2Win == 0) && (howManyTies == 0) && (howManyClicks == 0) ) {
+    else if ( (player1Win == 0) && (player2Win == 0) && (howManyTies == 0) && ($scope.boxesContainer.clickCounter == 0) ) {
         var playWithNew = confirm("Are you sure you wanna leave the game right now?\nYou don't even start it yet!\nClick \"OK\" to continue the game;\nClick \"Cancel\" to start a new game with someone else.");
         if (!playWithNew) {
             window.location.reload();
